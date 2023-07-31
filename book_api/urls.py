97d55c09bc5e67efafc512bbb536763c8d34f06b
okh-engineer <ohojimirzayev@gmail.com>
2023-07-31 15:48:55 +0500

    Delete files and audios urls

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
 from config import settings
-from .views import FileBookViewset, AudioBookViewset, AuthorViewset, FileBookListView, AudioBookListView
+from .views import FileBookViewset, AudioBookViewset, AuthorViewset
 from test import AddFiles
+from data.data import ADD_FILES_LINK
 
 if settings.DEBUG:
     router = routers.DefaultRouter()
@@ -14,7 +15,5 @@ router.register('audio-book-api', AudioBookViewset)
 router.register('author-api', AuthorViewset)
 urlpatterns = [
     path('', include(router.urls)),
-    path('files', FileBookListView.as_view(), name='files'),
-    path('audios', AudioBookListView.as_view(), name='audios'),
-    path('add-files', AddFiles, name='add-files'),
+    path(ADD_FILES_LINK, AddFiles, name='add-files'),
 ]
