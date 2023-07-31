561401b450e56ad6ffa027b5328e9b3104506d68
okh-engineer <ohojimirzayev@gmail.com>
2023-07-31 15:51:14 +0500

    Add "add-file-link" for add files and change database connection parameters

--- data/data.py
+++ data/data.py
@@ -6,5 +6,9 @@ load_dotenv()
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 DATABASE_USER = os.getenv('DATABASE_USER')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+
+ADD_FILES_LINK = os.getenv('ADD_FILES_LINK')
 
 korrektor_token = os.getenv('korrektor_token')
