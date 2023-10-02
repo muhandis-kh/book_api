2e4b4461dbe688f7837163ee20af3913d9fd5d4f
okh-engineer <ohojimirzayev@gmail.com>
2023-10-02 17:03:37 +0500

    Add files for upload to server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn "book_api.wsgi"
+web: gunicorn "config.wsgi"
--- config/settings.py
+++ config/settings.py
@@ -96,17 +96,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'book_api',
-#         'USER': 'postgres',
-#         'PASSWORD': 'obidcan124578',
-#         'HOST': localhost,
-#         'PORT': data.DATABASE_PORT,
-#     }
-# }
-
 
 
 # Password validation
@@ -145,8 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
